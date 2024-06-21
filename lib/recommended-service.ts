532add7bc9d10098dbaa7252c51e4a34194c16a7
d2verb <licprog73@gmail.com>
2024-06-21 18:06:43 +0900

    Don't show myself as recommemded user

--- lib/recommended-service.ts
+++ lib/recommended-service.ts
@@ -1,12 +1,36 @@
 import { db } from "@/lib/db";
+import { getSelf } from "@/lib/auth-service";
 
 export const getRecommended = async () => {
   await new Promise(resolve => setTimeout(resolve, 2000));
+  let userId;
 
-  const users = await db.user.findMany({
-    orderBy: {
-      createdAt: "desc"
-    },
-  });
+  try {
+    const self = await getSelf();
+    userId = self.id;
+  } catch {
+    userId = null;
+  }
+
+  let users = [];
+  if (userId) {
+    users = await db.user.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+      where: {
+        NOT: {
+          id: userId,
+        }
+      }
+    });
+  } else {
+    users = await db.user.findMany({
+      orderBy: {
+        createdAt: "desc"
+      },
+    });
+
+  }
   return users;
 };
