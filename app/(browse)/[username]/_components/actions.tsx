e5203cc9c3944f7067c33e39f3401c9db9a96f0c
d2verb <licprog73@gmail.com>
2024-06-21 23:53:23 +0900

    Can follow user now

---
+++ app/(browse)/[username]/_components/actions.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { toast } from "sonner";
+import { onFollow } from "@/actions/follow";
+import { Button } from "@/components/ui/button";
+import { useTransition } from "react";
+
+interface ActionsProps {
+  isFollowing: boolean;
+  userId: string;
+};
+
+export const Actions = ({
+  isFollowing,
+  userId
+}: ActionsProps) => {
+  const [isPending, startTransition] = useTransition();
+
+  const onClick = () => {
+    startTransition(() => {
+      onFollow(userId)
+        .then((data) => toast.success(`You are now following ${data.following.username}`))
+        .catch(() => toast.error("Something went wrong"));
+    });
+  }
+
+  return (
+    <Button
+      variant="primary"
+      onClick={onClick}
+      disabled={isFollowing || isPending}>
+      Follow
+    </Button>
+  );
+}
