93d30fc1eb0f0fba928a41dbbc6f6cdd03daa0ce
d2verb <licprog73@gmail.com>
2024-06-22 13:14:17 +0900

    Create block table

---
+++ prisma/migrations/20240622041348_create_block_table/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE `Block` (
+    `id` VARCHAR(191) NOT NULL,
+    `blockerId` VARCHAR(191) NOT NULL,
+    `blockedId` VARCHAR(191) NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    INDEX `Block_blockerId_idx`(`blockerId`),
+    INDEX `Block_blockedId_idx`(`blockedId`),
+    UNIQUE INDEX `Block_blockerId_blockedId_key`(`blockerId`, `blockedId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Block` ADD CONSTRAINT `Block_blockerId_fkey` FOREIGN KEY (`blockerId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Block` ADD CONSTRAINT `Block_blockedId_fkey` FOREIGN KEY (`blockedId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
