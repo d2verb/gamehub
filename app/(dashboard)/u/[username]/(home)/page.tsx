73a17697de91df4ab554f9ec62e2952e37a5da00
d2verb <licprog73@gmail.com>
2024-06-23 00:40:46 +0900

    Creator dashboard

---
+++ app/(dashboard)/u/[username]/(home)/page.tsx
@@ -0,0 +1,3 @@
+export default () => {
+  return <div>Creator Page</div>;
+};
---
+++ store/use-creator-sidebar.ts
@@ -0,0 +1,16 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+interface CreatorSidebarStore {
+  collapsed: boolean;
+  onExpand: () => void;
+  onCollapse: () => void;
+}
+
+export const useCreatorSidebar = create<CreatorSidebarStore>()(
+  devtools((set) => ({
+    collapsed: false,
+    onExpand: () => set(() => ({ collapsed: false })),
+    onCollapse: () => set(() => ({ collapsed: true })),
+  }))
+);
