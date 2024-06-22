1f8f70faee8a3870b9fb0373768bcf9144cfc0ea
d2verb <licprog73@gmail.com>
2024-06-23 02:24:28 +0900

    Remove fulltext index in Stream table because TiDB doesn't suppoert it.

--- prisma/migrations/20240622155243_create_stream_table/migration.sql
+++ prisma/migrations/20240622155243_create_stream_table/migration.sql
@@ -14,13 +14,14 @@ CREATE TABLE `Stream` (
     `createAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
+
     UNIQUE INDEX `Stream_ingressId_key`(`ingressId`),
     UNIQUE INDEX `Stream_userId_key`(`userId`),
     INDEX `Stream_userId_idx`(`userId`),
     INDEX `Stream_ingressId_idx`(`ingressId`),
-    FULLTEXT INDEX `Stream_name_idx`(`name`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Stream` ADD CONSTRAINT `Stream_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Stream`
+ADD CONSTRAINT `Stream_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
