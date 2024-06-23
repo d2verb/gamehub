4c164b16aca14f8542637e828b4e6806cd28f503
d2verb <licprog73@gmail.com>
2024-06-23 21:28:33 +0900

    Create media control

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -2,7 +2,10 @@
 
 import { useTracks } from "@livekit/components-react";
 import { Participant, Track } from "livekit-client";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
+import { useEventListener } from "usehooks-ts";
+import { FullscreenControl } from "./fullscreen-control";
+import { VolumeControl } from "./volume-control";
 
 interface LiveVideoProps {
   participant: Participant;
@@ -11,6 +14,46 @@ interface LiveVideoProps {
 export const LiveVideo = ({ participant }: LiveVideoProps) => {
   const videoRef = useRef<HTMLVideoElement>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
+
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const [volume, setVolume] = useState(0);
+
+  const onVolumeChange = (value: number) => {
+    setVolume(+value);
+    if (videoRef?.current) {
+      videoRef.current.muted = value === 0;
+      videoRef.current.volume = +value * 0.01;
+    }
+  };
+
+  const toggleMute = () => {
+    const isMuted = volume === 0;
+    setVolume(isMuted ? 50 : 0);
+
+    if (videoRef?.current) {
+      videoRef.current.muted = !isMuted;
+      videoRef.current.volume = isMuted ? 0.5 : 0;
+    }
+  };
+
+  useEffect(() => {
+    onVolumeChange(0);
+  }, []);
+
+  const toggleFullscreen = () => {
+    if (isFullscreen) {
+      document.exitFullscreen();
+    } else if (wrapperRef?.current) {
+      wrapperRef.current.requestFullscreen();
+    }
+  };
+
+  // Handle fullscreen change events (like pressing ESC to exit fullscreen)
+  const handleFullscreenChange = () => {
+    const isCurrentlyFullscreen = document.fullscreenElement !== null;
+    setIsFullscreen(isCurrentlyFullscreen);
+  };
+  useEventListener("fullscreenchange", handleFullscreenChange, wrapperRef);
 
   useTracks(Object.values(Track.Source))
     .filter((track) => track.participant.identity === participant.identity)
@@ -23,6 +66,16 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
   return (
     <div ref={wrapperRef} className="relative h-full flex">
       <video ref={videoRef} width="100%" />
+      <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
+        <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
+          <VolumeControl
+            value={volume}
+            onToggle={toggleMute}
+            onChange={onVolumeChange}
+          />
+          <FullscreenControl isFullscreen={false} onToggle={toggleFullscreen} />
+        </div>
+      </div>
     </div>
   );
 };
