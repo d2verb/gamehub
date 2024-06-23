9313460bb4f98c8dc6500099ddd10161dbc6ae8d
d2verb <licprog73@gmail.com>
2024-06-24 01:35:34 +0900

    Create chat sidebar

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -2,8 +2,12 @@
 
 import { useViwerToken } from "@/hooks/use-viewer-token";
 import { Stream, User } from "@prisma/client";
+import { cn } from "@/lib/utils";
+import { useChatSidebar } from "@/store/use-chat-sidebar";
 import { LiveKitRoom } from "@livekit/components-react";
 import { Video } from "./video";
+import { Chat } from "./chat";
+import { ChatToggle } from "./chat-toggle";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -17,6 +21,7 @@ export const StreamPlayer = ({
   isFollowing,
 }: StreamPlayerProps) => {
   const { token, name, identity } = useViwerToken(user.id);
+  const { collapsed } = useChatSidebar((state) => state);
 
   if (!token || !name || !identity) {
     return <div>Cannot watch the stream</div>;
@@ -24,13 +29,33 @@ export const StreamPlayer = ({
 
   return (
     <>
+      {collapsed && (
+        <div className="hidden lg:block fixed top-[66px] right-2 z-50">
+          <ChatToggle />
+        </div>
+      )}
       <LiveKitRoom
         token={token}
         serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
-        className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+        className={cn(
+          "grid grid-cols-1 lg:gap-y-0 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5 h-full",
+          collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
+        )}
       >
-        <div className="space-y-4 col-span-1 lg:col-span2 xl:col-span-2 2xl:col-span-5 lg:opverflow-y-auto hidden-scrollbar pb-10">
+        <div className="space-y-4 col-span-1 lg:col-span-1 xl:col-span-2 2xl:col-span-4 lg:opverflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user.username} hostIdentity={user.id} />
+        </div>
+
+        <div className={cn("col-span-1", collapsed && "hidden")}>
+          <Chat
+            viewerName={name}
+            hostName={user.username}
+            hostIdentity={user.id}
+            isFollowing={isFollowing}
+            isChatEnabled={stream.isChatEnabled}
+            isChatDelayed={stream.isChatDelayed}
+            isChatFollowersOnly={stream.isChatFollowersOnly}
+          />
         </div>
       </LiveKitRoom>
     </>
---
+++ store/use-chat-sidebar.ts
@@ -0,0 +1,25 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+export enum ChatVariant {
+  CHAT = "CHAT",
+  COMMUNITY = "COMMUNITY",
+}
+
+interface ChatSidebarStore {
+  collapsed: boolean;
+  variant: ChatVariant;
+  onExpand: () => void;
+  onCollapse: () => void;
+  onChangeVariant: (variant: ChatVariant) => void;
+}
+
+export const useChatSidebar = create<ChatSidebarStore>()(
+  devtools((set) => ({
+    collapsed: false,
+    variant: ChatVariant.CHAT,
+    onExpand: () => set(() => ({ collapsed: false })),
+    onCollapse: () => set(() => ({ collapsed: true })),
+    onChangeVariant: (variant: ChatVariant) => set(() => ({ variant })),
+  }))
+);
