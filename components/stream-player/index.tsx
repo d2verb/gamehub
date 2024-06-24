3842f0b5a3d9c805380949122ea4b674fd47a27e
d2verb <licprog73@gmail.com>
2024-06-25 00:08:36 +0900

    Create stream header

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -9,6 +9,7 @@ import { Video, VideoSkeleton } from "./video";
 import { Chat, ChatSkeleton } from "./chat";
 import { ChatToggle } from "./chat-toggle";
 import { Skeleton } from "@/components/ui/skeleton";
+import { Header, HeaderSkeleton } from "./header";
 
 interface StreamPlayerProps {
   user: User & { stream: Stream | null };
@@ -45,6 +46,14 @@ export const StreamPlayer = ({
       >
         <div className="space-y-4 col-span-1 lg:col-span-1 xl:col-span-2 2xl:col-span-4 lg:opverflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user.username} hostIdentity={user.id} />
+          <Header
+            hostName={user.username}
+            hostIdentity={user.id}
+            viewerIdentity={identity}
+            imageUrl={user.imageUrl}
+            isFollowing={isFollowing}
+            name={stream.name}
+          />
         </div>
 
         <div className={cn("col-span-1", collapsed && "hidden")}>
@@ -68,6 +77,7 @@ export const StreamPlayerSkeleton = () => {
     <div className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5 h-full">
       <div className="space-y-4 col-span-1 lg:col-span-1 xl:col-span-2 2xl:col-span-4 lg:opverflow-y-auto hidden-scrollbar pb-10">
         <VideoSkeleton />
+        <HeaderSkeleton />
       </div>
       <div className="col-span-1 bg-background">
         <ChatSkeleton />
