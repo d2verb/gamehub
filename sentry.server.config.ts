e3491b7232526d0b535129052ab9621b62c10a59
d2verb <licprog73@gmail.com>
2024-07-05 00:55:18 +0900

    Change transaction sample rate

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -8,12 +8,11 @@ Sentry.init({
   dsn: "https://8b0e15ca758ff2c29823a63ef19925ea@o4507543979294720.ingest.us.sentry.io/4507543982833664",
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.2,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
   // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: process.env.NODE_ENV === 'development',
-  
 });
